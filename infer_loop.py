9135af405d097515ad3c4e298f1459cb62aedf6e
BasselSh <basselalshawareb1@gmail.com>
2025-02-21 12:46:29 +0300

    Reorganized the code. Added parser

--- infer_loop.py
+++ infer_loop.py
@@ -5,43 +5,60 @@ import os
 import subprocess
 from sentence_transformers import SentenceTransformer
 import os
+import argparse
 
-# Expand the tilde to the full path
-conda_env_python = os.path.expanduser('~/anaconda3/envs/lavis/bin/python')
+def parse_args():
+    parser = argparse.ArgumentParser(description="Process an image file.")
+    parser.add_argument('prompt_file', type=str, help='Path to the prompt file to be processed')
+    parser.add_argument('lavis_env', type=str, help='Path to the lavis environment')
+    parser.add_argument('--sd-model', type=str, default='stable-diffusion-v1-5/stable-diffusion-v1-5', help='Path to the model file to be processed')
+    parser.add_argument('--lora_path', type=str, default='sd-model-finetuned-lora/checkpoint-6000', help='Path to the lora file to be processed')
+    parser.add_argument('--num_images', type=int, default=10, help='Number of images to be generated')
+    parser.add_argument('--num_attempts', type=int, default=4, help='Number of attempts to generate an image')
+    return parser.parse_args()
 
-# 1. Load a pretrained Sentence Transformer model
-cos_model = SentenceTransformer("all-MiniLM-L6-v2")
+def inference_loop(classes, num_images, num_attempts, cos_model, diffuser, lavis_env):
+     os.makedirs('generated_images', exist_ok=True)
+     for class_idx, line in enumerate(classes):
+        category = line.split(',')[0]
+        os.makedirs(f'generated_images/{category}', exist_ok=True)
+        os.makedirs(f'generated_images/{category}/correct', exist_ok=True)
+        os.makedirs(f'generated_images/{category}/incorrect', exist_ok=True)
+        prompt = f'An underwater view of {category}'
+        for j in range(num_images):
+            for attempt in range(num_attempts):
+                image_pred = diffuser(prompt, num_inference_steps=30).images[0] #, generator = torch.Generator(device='cuda')
+                image_pred.save('tmp.jpg')
+                caption = subprocess.run([lavis_env, 'blip_infer.py'], capture_output=True, text=True).stdout[:-1]
+                embeddings = cos_model.encode([prompt, caption])
+                similarity = cos_model.similarity(embeddings[0], embeddings[1]).squeeze().item()
+                print(f"Prompt: {prompt}. Caption: {caption}. Sim: {similarity}")
+                if similarity > 0.7:
+                    image_pred.save(f'generated_images/{category}/correct/{category}_{j}_attempt{attempt}.jpg')
+                    break
+                else:
+                    image_pred.save(f'generated_images/{category}/incorrect/{category}_{j}_attempt{attempt}.jpg')
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def main():
+    args = parse_args()
+    prompt_file = args.prompt_file
+    num_images = args.num_images
+    num_attempts = args.num_attempts
+    sd_model = args.sd_model
+    lora_dir = args.lora_path
+    lavis_env = os.path.expanduser(args.lavis_env)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-pipeline_path = "stable-diffusion-v1-5/stable-diffusion-v1-5"
-tmpdir = 'sd-model-finetuned-lora/checkpoint-6000'
-diffuser = DiffusionPipeline.from_pretrained(pipeline_path)
-diffuser.load_lora_weights(tmpdir)
-diffuser.to(device)
+    cos_model = SentenceTransformer("all-MiniLM-L6-v2")
+    diffuser = DiffusionPipeline.from_pretrained(sd_model)
+    diffuser.load_lora_weights(lora_dir)
+    diffuser.to(device)
+    cos_model.to(device)
+    with open(prompt_file, 'r') as f:
+        lines = f.read().split('\n')
 
-with open('imagenet_classes.txt', 'r') as f:
-    lines = f.read().split('\n')
+    inference_loop(lines, num_images, num_attempts, cos_model, diffuser, lavis_env)
+        
 
-for i, line in enumerate(lines):
-    if i < 15: continue
-    category = line.split(',')[0]
-    os.makedirs(f'generated_images/{category}', exist_ok=True)
-    os.makedirs(f'generated_images/{category}/correct', exist_ok=True)
-    os.makedirs(f'generated_images/{category}/incorrect', exist_ok=True)
-    prompt = f'An underwater view of {category}'
-    for i in range(1):
-        for attempt in range(4):
-            image_pred = diffuser(prompt, num_inference_steps=30).images[0] #, generator = torch.Generator(device='cuda')
-            image_pred.save('tmp.jpg')
-            caption = subprocess.run([conda_env_python, '/home/human/bassel/underwater/diffusers/blip_infer.py'], capture_output=True, text=True).stdout[:-1]
-            embeddings = cos_model.encode([prompt, caption])
-            similarity = cos_model.similarity(embeddings[0], embeddings[1]).squeeze().item()
-            print(f"Prompt: {prompt}. Caption: {caption}. Sim: {similarity}")
-            if similarity > 0.7:
-                image_pred.save(f'generated_images/{category}/correct/{category}_{i}_attempt{attempt}.jpg')
-                break
-            else:
-                image_pred.save(f'generated_images/{category}/incorrect/{category}_{i}_attempt{attempt}.jpg')
-    
-
+if __name__ == "__main__":
+    main()
